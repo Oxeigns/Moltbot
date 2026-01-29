627869ba76f352ad5a84c1fe428ecae2f97e8773
Yash Samrat <yashsamrat10080@gmail.com>
2026-01-29 16:22:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+set -e
 
+echo "Starting Moltbot Gateway..."
+
+# ---- Disable clipboard native crash ----
 CLIP_PATH="node_modules/@mariozechner/clipboard/index.js"
 
 if [ -f "$CLIP_PATH" ]; then
-  echo "module.exports = { writeSync(){}, readSync(){ return '' } }" > $CLIP_PATH
+  echo "module.exports = { writeSync(){}, readSync(){ return '' } }" > "$CLIP_PATH"
+  echo "Clipboard module patched"
 fi
 
+# ---- Environment ----
 export NODE_ENV=production
 export TERM=dumb
 
-./node_modules/.bin/clawdbot gateway run \
---allow-unconfigured \
---no-ui \
---headless \
---log-level info
+# ---- Start Gateway (no unsupported flags) ----
+./node_modules/.bin/clawdbot gateway run
